b4552906560bb3869c1080e8c58775f2cee1096e
TextAdventure <roehrle@rhrk.uni-kl.de>
2015-01-01 18:27:46 +0100

    Displays umbenannt und die Listener

--- util/InventoryListener.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-/**
- * Empfaengt InventoryEvents, die dann verarbeitet werden koennen
- */
-public interface InventoryListener {
-	// Diese Methode muss ueberschrieben werden.
-	public void inventoryUpdate(InventoryEvent evt);
-}
--- util/PlayerListener.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-/**
- * Ein Listener, der Spielerveraenderungen wahrnimmt.
- */
-public interface PlayerListener {
-	// Diese Methode muss ueberschrieben werden.
-	public abstract void playerChanged(PlayerEvent evt); 
-}
